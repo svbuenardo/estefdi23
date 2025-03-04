dd06cc871dabb4d613a661452e731adc1f26393c
svbuenardo <svbuenoyoqse@gmail.com>
2025-03-04 17:46:20 +0000

    [🌙] Backup realizado 2025-03-04

--- servidor_minecraft/run.bat
+++ servidor_minecraft/run.bat
@@ -1,15 +1,6 @@
-@echo off
-REM Add custom JVM arguments (such as RAM allocation) to the user_jvm_args.txt
-
-java -jar forge-1.20.4-49.1.4-shim.jar --onlyCheckJava
-if %ERRORLEVEL% NEQ 0 (
-    echo.
-    echo If you're struggling to fix the error above, ask for help on the forums or Discord mentioned in the readme.
-    goto :exit
-)
-
-REM Add custom program arguments (such as nogui) to the next line before the %* or pass them to this script directly
-java @libraries/net/minecraftforge/forge/1.20.4-49.1.4/win_args.txt %*
-
-:exit
+REM Forge requires a configured set of both JVM and program arguments.
+REM Add custom JVM arguments to the user_jvm_args.txt
+REM Add custom program arguments {such as nogui} to this file in the next line before the %* or
+REM  pass them to this script directly
+java @user_jvm_args.txt -XX:+UnlockExperimentalVMOptions -XX:+UnlockDiagnosticVMOptions -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+UseNUMA -XX:NmethodSweepActivity=1 -XX:ReservedCodeCacheSize=400M -XX:NonNMethodCodeHeapSize=12M -XX:ProfiledCodeHeapSize=194M -XX:NonProfiledCodeHeapSize=194M -XX:-DontCompileHugeMethods -XX:MaxNodeLimit=240000 -XX:NodeLimitFudgeFactor=8000 -XX:+UseVectorCmov -XX:+PerfDisableSharedMem -XX:+UseFastUnorderedTimeStamps -XX:+UseCriticalJavaThreadPriority -XX:ThreadPriorityPolicy=1 -XX:AllocatePrefetchStyle=3  -XX:+UseG1GC -XX:MaxGCPauseMillis=37 -XX:+PerfDisableSharedMem -XX:G1HeapRegionSize=16M -XX:G1NewSizePercent=23 -XX:G1ReservePercent=20 -XX:SurvivorRatio=32 -XX:G1MixedGCCountTarget=3 -XX:G1HeapWastePercent=20 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1RSetUpdatingPauseTimePercent=0 -XX:MaxTenuringThreshold=1 -XX:G1SATBBufferEnqueueingThresholdPercent=30 -XX:G1ConcMarkStepDurationMillis=5.0 -XX:G1ConcRSHotCardLimit=16 -XX:G1ConcRefinementServiceIntervalMillis=150 -XX:GCTimeRatio=99 -XX:+UseLargePages -XX:LargePageSizeInBytes=2m @libraries/net/minecraftforge/forge/1.18.2-40.2.4/win_args.txt nogui %*
 pause
